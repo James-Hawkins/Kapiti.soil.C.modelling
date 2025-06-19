a98e8d105d5b3f0c7d6c4e274b304464a5e18abe
James Hawkins <jameshawkins702@gmail.com>
2025-06-19 13:26:39 +0300

    @_@

--- projects/KE_Kapiti_Base/KE_Kapiti_eddy.bat
+++ projects/KE_Kapiti_Base/KE_Kapiti_eddy.bat
@@ -1,17 +1,17 @@
 set echo off
 
-C:\Users\hawkj\Documents\Github\L-DNDC\LandscapeDNDC\ldndc\ldndc.exe -c C:\Users\hawkj\.ldndc\ldndc.conf KE_Kapiti_eddy.ldndc
+C:\Users\hawkj\Documents\Github\L-DNDC\Landscape-DNDC\ldndc\ldndc.exe -c C:\Users\hawkj\.ldndc\ldndc.conf KE_Kapiti_eddy.ldndc
 
 
 set "inputFile=KE_Kapiti_output_eddy\KE_Kapiti_soilchemistry-daily.txt"
 set "inputFile2=KE_Kapiti_output_eddy\KE_Kapiti_physiology-daily.txt"
 set "inputFile3=KE_Kapiti_output_eddy\KE_Kapiti_watercycle-daily.txt"
-#set "inputFile4=KE_Kapiti_climate_eddy - JH+date2019-2021.csv"
+set "inputFile4=KE_Kapiti_climate_eddy_2019_336_onwards.txt"
 
 set "outputFile=KE_Kapiti_output_eddy\KE_Kapiti_soilchemistry-daily.csv"
 set "outputFile2=KE_Kapiti_output_eddy\KE_Kapiti_physiology-daily.csv"
 set "outputFile3=KE_Kapiti_output_eddy\KE_Kapiti_watercycle-daily.csv"
-#set "outputFile4=KE_Kapiti_output_eddy\KE_Kapiti_climate_eddy - JH+date2019-2021.csv"
+set "outputFile4=KE_Kapiti_output_eddy\KE_Kapiti_climate_eddy_2019_336_onwards.csv"
 
 
 
