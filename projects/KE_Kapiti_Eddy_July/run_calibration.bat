65ba7efff9f6439ee9af0249e55f95d560ed30c0
James Hawkins <jameshawkins702@gmail.com>
2026-07-25 13:03:18 +0300

    _

---
+++ projects/KE_Kapiti_Eddy_July/run_calibration.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\JHawkins\Documents\Github\Soil.C.modelling\Kapiti.soil.C\projects\KE_Kapiti_Eddy_July"
+"C:\Program Files\R\R-4.5.0\bin\x64\Rscript.exe" auto_calibrate.R
+pause
